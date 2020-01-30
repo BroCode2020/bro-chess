053809ac993adf5a282ad444822dfc35c500efac
Eric Whitehead <ericwhitehead77@gmail.com>
2020-01-30 06:15:38 +0000

    added game and piece model, controller

--- db/migrate/20200128074241_create_games.rb
+++ db/migrate/20200128074241_create_games.rb
@@ -2,6 +2,11 @@ class CreateGames < ActiveRecord::Migration[5.2]
   def change
     create_table :games do |t|
       t.string :name
+      t.integer :white_player_id
+      t.integer :black_payer_id
+      t.datetime :created_at
+      t.datetime :updated_at
+
       t.timestamps
     end
   end
