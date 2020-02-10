dba3fc53ffab342e5bb4ff8698649fa638d8bfc5
Eric Whitehead <ericwhitehead77@gmail.com>
2020-02-10 19:38:50 +0000

    Setup controller actions for moving pieces

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -15,6 +15,26 @@ class GamesController < ApplicationController
     @game = Game.find(params[:id])
   end
 
+  def select
+    @game = Game.find(params[:id])
+    @pieces = @game.pieces
+    @piece = Piece.find(params[:piece_id])
+    @piece_id = params[:piece_id]
+    @x_pos = params[:x_pos]
+    @y_pos = params[:y_pos]
+  end
+
+  def move
+    @game = Game.find(params[:id])
+    @pieces = @game.pieces
+    @piece = Piece.find(params[:piece_id])
+    @piece_id = params[:piece_id]
+    @x_pos = params[:x_pos]
+    @y_pos = params[:y_pos]
+    @piece.update_attributes({:x_pos => @x_pos, :y_pos => @y_pos})
+    redirect_to game_path(@game)
+  end
+
   private
 
   def game_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  root 'static_pages#index'
   resources :games
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  get 'games/:id/:piece_id/:x_coord/:y_coord', :to => 'games#select', :as => 'select'
+  post 'games/:id/:piece_id/:x_coord/:y_coord', :to => 'games#move', :as => 'move'
+
+  root 'static_pages#index'
+
 end
