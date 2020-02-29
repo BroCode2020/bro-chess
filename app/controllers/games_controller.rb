80842e29652dd77d9d4e0b8df521cf38ff91c455
Eric Whitehead <ericwhitehead77@gmail.com>
2020-02-29 02:34:00 +0000

    got dragging and dropping and hover highlighting to work fairly well

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -39,8 +39,8 @@ class GamesController < ApplicationController
     @piece_id = params[:piece_id]
     @x_pos = params[:x_pos]
     @y_pos = params[:y_pos]
-    #<!-- @piece.move_to! -->
-    @piece.update_attributes({:x_pos => @x_pos, :y_pos => @y_pos})
+    @piece.move_to!(@x_pos, @y_pos)
+    #@piece.update_attributes({:x_pos => @x_pos, :y_pos => @y_pos})
     redirect_to game_path(@game.id)
   end
 
