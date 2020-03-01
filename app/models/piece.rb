22403004f0d26a4c0e53de887d4444c72903728d
dmstoker <dmstoker88@gmail.com>
2020-03-01 07:45:52 +0000

    resloved merge conflicts

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -7,7 +7,6 @@ class Piece < ApplicationRecord
 
 
   def move_to!(new_x, new_y)
-    self.update_attributes(:moved => true)
     piece_to_capture = self.game.pieces.where(:x_pos => new_x, :y_pos => new_y).first
 		if piece_to_capture.present? && self.color.to_i != piece_to_capture.color.to_i
 			piece_to_capture.update_attributes(:x_pos => nil, :y_pos => nil) #captured pieces are nil thus not drawn and not clickable
