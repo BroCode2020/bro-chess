f4994a2d34226b7e564b70775858211b54aba5af
Julian Smith <smith.julian.f@gmail.com>
2020-03-06 19:09:53 +0000

    Converted params from string to ints in move_to, fixing the biggest error

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -13,10 +13,11 @@ class Piece < ApplicationRecord
   end
 
 
-   def move_to!(new_x, new_y)
+  def move_to!(new_x, new_y)
+    new_x = new_x.to_i
+    new_y = new_y.to_i
     if valid_move?(new_x, new_y)
       piece_to_capture = self.game.pieces.where(:x_pos => new_x, :y_pos => new_y).first
-
       if piece_to_capture.present? && self.color.to_i != piece_to_capture.color.to_i
         piece_to_capture.update_attributes(:x_pos => nil, :y_pos => nil) #captured pieces are nil thus not drawn and not clickable
         self.update_attributes(:x_pos => new_x, :y_pos => new_y)
