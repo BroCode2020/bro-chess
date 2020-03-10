7e09bab9571e1e09ec1bde3d1e90864f74b83131
Philip Cox <philcogm@gmail.com>
2020-03-10 19:55:56 +0000

    created user dashboard, added link to navbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_26_015548) do
+ActiveRecord::Schema.define(version: 2020_03_10_173017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2020_02_26_015548) do
     t.datetime "updated_at", null: false
     t.integer "black_player_id"
     t.integer "forfeiting_player_id"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_games_on_user_id"
   end
 
   create_table "pieces", force: :cascade do |t|
@@ -47,7 +49,9 @@ ActiveRecord::Schema.define(version: 2020_02_26_015548) do
     t.string "uid"
     t.string "name"
     t.text "image"
+    t.integer "game_id"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["game_id"], name: "index_users_on_game_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
