216c026aaaa1ac700859054f1eff050884ccb6c1
dmstoker <dmstoker88@gmail.com>
2020-03-11 09:53:26 +0000

    fixed king can castle bug

---
+++ db/migrate/20200311070825_add_last_move_piece_id_to_games.rb
@@ -0,0 +1,9 @@
+class AddLastMovePieceIdToGames < ActiveRecord::Migration[5.2]
+  def change
+ add_column :games, :last_moved_piece_id, :integer
+ add_foreign_key "games", "pieces", name: "games_last_moved_piece_fk", column: "last_moved_piece_id"
+ add_column :games, :last_moved_prev_x_pos, :integer
+ add_column :games, :last_moved_prev_y_pos, :integer
+ remove_column :games, :piece_capturable_by_en_passant, :string
+end
+  end
