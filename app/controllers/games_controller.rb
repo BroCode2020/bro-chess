ac26b2567497b0c983f7fe9683ce929646a59c17
Julian Smith <smith.julian.f@gmail.com>
2020-03-16 15:01:36 +0000

    Fixed oversight in controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -88,6 +88,8 @@ class GamesController < ApplicationController
   end
 
   def forfeit
+    @game = Game.find(params[:id])
+
     if(current_user.id != @game.black_player_id && current_user.id != @game.white_player_id)
       redirect_to root_path, alert: "You are not a member of this game." and return
     end
