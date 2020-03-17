9acc5c1879b43bf3e0faa04a7014ebfdba61e3d3
Julian Smith <smith.julian.f@gmail.com>
2020-03-17 18:23:25 +0000

    Corrected approach definition in ViewBro

--- lib/assets/view_bro.rb
+++ lib/assets/view_bro.rb
@@ -1,21 +1,32 @@
 class ViewBro
-	attr_reader :msg_for_already_forfeited
-	attr_reader :msg_for_forfeited_game
-	attr_reader :msg_for_game_non_member
-	attr_reader :msg_for_moving_into_check
-	attr_reader :msg_for_moving_outside_of_turn
-	attr_reader :msg_for_not_signed_in
 
-	attr_reader :svg_scale
+	def self.msg_for_already_forfeited
+		return 'This game has already been forfeited.'
+	end
 
-	@@msg_for_already_forfeited = "This game has already been forfeited."
-	@@msg_for_forfeited_game = "You have forfeited the game. Please play again soon."
-	@@msg_for_game_non_member = "You are not a member of this game."
-	@@msg_for_moving_into_check = "You cannot move into check."
-	@@msg_for_moving_outside_of_turn = "You can only move on your turn."
-	@@msg_for_not_signed_in = "You need to sign in or sign up before continuing."
+	def self.msg_for_forfeited_game
+		return 'You have forfeited the game. Please play again soon.'
+	end
 
-	@@svg_scale = 45
+	def self.msg_for_game_non_member
+		return 'You are not a member of this game.'
+	end
+
+	def self.msg_for_moving_into_check
+		return 'You cannot move into check.'
+	end
+
+	def self.msg_for_moving_outside_of_turn
+		return 'You can only move on your turn.'
+	end
+
+	def self.msg_for_not_signed_in
+		return 'You need to sign in or sign up before continuing.'
+	end
+
+	def self.svg_scale 
+		return 45
+	end
 
 	def self.from_0_to_7 (reverse_order)
 		return (0..7).to_a.reverse if reverse_order
@@ -44,12 +55,12 @@ class ViewBro
 		  'INVALID PIECE'
 		end
 	
-		x_coord = @@svg_scale * column_number
-		y_coord = color == 1 ? 0 : @@svg_scale
+		x_coord = svg_scale * column_number
+		y_coord = color == 1 ? 0 : svg_scale
 	
 		data_string = svg_url +'#svgView(viewBox('
 		data_string += x_coord.to_s + ', ' + y_coord.to_s + ', '
-		data_string += @@svg_scale.to_s + ', ' + @@svg_scale.to_s + '))'
+		data_string += svg_scale.to_s + ', ' + svg_scale.to_s + '))'
 	
 		return data_string
 	  end
