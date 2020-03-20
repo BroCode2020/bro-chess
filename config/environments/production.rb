bb826e4e1b5aabf14abdca0e83903df67e6a479c
Julian Smith <smith.julian.f@gmail.com>
2020-03-20 00:08:03 +0000

    Changed config options for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.serve_static_assets = true
+
   config.action_mailer.default_url_options = { host: 'bro-chess.herokuapp.com/' }
 
   # Code is not reloaded between requests.
